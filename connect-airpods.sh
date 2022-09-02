6fbac115c804e203c4de0028681ceff7cb29178a
Takuro Matsukawa <takuro.matsukawa@gmail.com>
2022-09-02 10:14:31 +0900

    Update airpods name

--- connect-airpods.sh
+++ connect-airpods.sh
@@ -9,7 +9,7 @@
 # @raycast.icon 🤖
 
 AIR_PODS_ADDRESS=28-f0-33-88-27-d1 # Your AirPods MAC address
-AIR_PODS_NAME="Takuro’s AirPods Pro" # Your AirPods name
+AIR_PODS_NAME="Tak’s AirPods Pro" # Your AirPods name
 
 bluetoothconnector -c "$AIR_PODS_ADDRESS"
 for ((i=0 ; i<10 ; i++))
